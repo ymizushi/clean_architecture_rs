2896c69c963e8dca916ba2ac146018b8d50e5897
ymizushi <mizushi@gmail.com>
2020-07-01 23:17:13 +0900

    fix user dao

--- src/adapter/controller/user_controller.rs
+++ src/adapter/controller/user_controller.rs
@@ -1,14 +1,16 @@
-use crate::adapter::repository::user_dao::TestUserRepository;
+use crate::adapter::repository::user_dao::UserRepositoryBySQLite;
 
 use crate::usecase::user_creation::UserOperation;
+
+use crate::entity::user::UserID;
 
 use actix_web::{get, web, Responder};
 
 #[get("/users/{id}")]
-pub async fn show(info: web::Path<(i32)>) -> impl Responder {
-    let user_creation =  UserOperation { ur: TestUserRepository {} };
+pub async fn show(info: web::Path<i64>) -> impl Responder {
+    let user_operation =  UserOperation { ur: UserRepositoryBySQLite {} };
 
-    if let Some(user) = user_creation.create() {
+    if let Some(user) = user_operation.find(UserID { id: info.into_inner() }) {
         format!("Hello {}", user.name)
     } else {
         format!("bye")
--- src/usecase/user_creation.rs
+++ src/usecase/user_creation.rs
@@ -1,4 +1,4 @@
-use crate::entity::user::User;
+use crate::entity::user::{User, UserID};
 use super::gateway::repository::user_repository::UserRepository;
 
 pub struct UserOperation<U: UserRepository> {
@@ -8,5 +8,8 @@ pub struct UserOperation<U: UserRepository> {
 impl<U: UserRepository> UserOperation<U> {
     pub fn create(&self) -> Option<User> {
         self.ur.create()
+    }
+    pub fn find(&self, id: UserID) -> Option<User> {
+        self.ur.find(id)
     }
 }
